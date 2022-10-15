f17998f9c09ade711a50fc22c55f95784b2ac612
Alberto <albertiae.digital@gmail.com>
2022-10-15 16:12:12 -0400

    Created the file main.js

---
+++ Dump-Truck-Loading-Animation/main.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", loading)
+function loading() {
+  var percents = [0.25,0.5,0.75,1],
+      steps = 0,
+      truckLoopDur = 10,
+      fill = function() {
+        let fillEl =
+        document.querySelector(".progress-fill");
+        fillEl.style.transform = "scaleY("
+        + percents[steps] + ")";
+        ++ step;
+
+        if (step < percents.length) {
+          setTimeout(fill, (truckLoopDur * 1e3)/2);
+        }
+      };
+      setTimeout(fill, (truckLoopDur * 1e3)/4);
+}
